f2b6f26c584ef2a6354f2456a79b181d37f815c0
elepshchikova <elepshchikova@mail.ru>
2022-09-05 22:39:15 +0300

    решение задачи

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 7;
+
+if (a > b){
+    System.Console.WriteLine("Число А больше числа Б");
+}
+else{
+    if (b > a){
+        System.Console.WriteLine("Число Б больше числа А");
+    }
+    else{
+        System.Console.WriteLine("Числа равны");
+    }
+ }
+  
+
+System.Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
